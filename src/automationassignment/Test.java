0272ee80888ed0dd57127804c85427e8817ee3da
PratapChauhan <PratapChauhan@192.168.1.102>
2023-10-08 19:15:45 +0530

    Temperature file code push

--- src/automationassignment/Test.java
+++ src/automationassignment/Test.java
@@ -3,8 +3,146 @@ package automationassignment;
 public class Test {
 	public static void main(String[] args)
 	{
-		System.out.println("HEllo World");
+		//int in = 1234_23;
+		//System.out.println(in);
+		//byte b1 = (byte)250;
+		//System.out.println(b1);
 		
+		//byte b1 = (byte)120;
+		//System.out.println(b1);
+		
+		//byte b1 = (byte)130;
+		//System.out.println(b1);
+		
+		
+		//byte b1 = (byte)300;
+		//System.out.println(b1);
+		
+		
+	//	int a = 20;
+		//    long b = a;     //implicit casting from int to long data type
+		//double c = b;   // implicit casting from long to double data type
+
+		//System.out.println(a);
+		//System.out.println(b);
+		//System.out.println(c);
+		
+		//byte b1 = 128;
+	
+		//byte b1 = (byte) b1;
+		
+		//byte b1 = (byte)(b1*3);
+		//System.out.println(b1);
+		
+	/*	int x = 1;
+
+	      int y = 0;
+
+	      if (x++ > ++y) {
+
+	          System.out.print("Hello");
+
+	      }else {
+
+	          System.out.print("Welcome");
+
+	      }
+
+	      System.out.print("Log" + x + ":" + y);
+
+		
+		
+		int i1 = 10;
+
+		int j = i1++ + ++i1;
+				//10 + 12
+		System.out.println(j);
+
+
+
+		int i2 = 10;
+
+		int k = ++i2 + ++i2 + ++i2 + i2++;
+				//11 +12+ 13 + 14 
+		System.out.println(k);
+
+
+
+		int i3 = 10;
+
+		int m = ++i3 + i3++ + i3++ + i3++  + i3-- + i3-- + --i3;
+				//11 + 11+	   12 +   13+    14 + 13 - 12
+
+		System.out.println(m); 
+	
+		int i = 10;
+
+		i = i++;
+
+		System.out.println(i);
+
+		int j = 10;
+
+		j = j++ + j;
+
+		System.out.println(j); 
+		   int i = 10;   
+
+		   i = i++ + i + ++i ;
+
+		   System.out.println(i);
+		
+		int i1 = -10;
+
+	      System.out.println(i1);
+
+	      
+
+	      int i2 = 10;
+
+	      System.out.println(i2++);
+
+
+
+	      int i3 = 10;
+
+	      System.out.println(++i3);
+
+
+
+	      int i4 = 10;
+
+	      System.out.println(i4--);
+
+
+
+	      int i5 = 10;
+
+	      System.out.println(--i5);
+
+	  
+
+	      int i6 = 10;
+
+	      System.out.println(~i6); */
+		
+		
+		double db1 = 14.5d;
+
+		db1++;
+
+		System.out.println(db1);
+
+		final int i = 10;
+
+		//i++;
+
+		System.out.println(i);
+
+		boolean bool = true;
+
+	//	bool++;
+		System.out.println(bool);
 	}
 
 }
