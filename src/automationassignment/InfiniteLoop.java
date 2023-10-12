954788759c8b1da0fd1905db597aef70b58dad56
PratapChauhan <PratapChauhan@host.docker.internal>
2023-10-12 12:03:51 +0530

    InfiniteLoop file committed

---
+++ src/automationassignment/InfiniteLoop.java
@@ -0,0 +1,23 @@
+package automationassignment;
+
+public class InfiniteLoop
+{
+	public static void main(String[] args)
+
+	{
+	//	while(true)
+		//System.out.println("This is while infiteloop ");
+	
+	
+	//for(int i=0; i<3; --i)
+		//System.out.println("This is For infiteloop ");
+	
+	
+	do
+		{
+			System.out.println("This is DO while infiteloop ");
+			
+		}while (true);
+	}
+
+}
