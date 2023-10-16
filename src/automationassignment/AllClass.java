6f25d8274f9972a4736065a98b8ec620c39b858e
PratapChauhan <PratapChauhan@host.docker.internal>
2023-10-16 10:48:04 +0530

    AllClass file committed

---
+++ src/automationassignment/AllClass.java
@@ -0,0 +1,97 @@
+package automationassignment;
+
+public class AllClass {
+
+}
+
+class IndHuman {
+
+String name;
+int age;
+double height;
+double weght;
+static String country;
+
+}
+
+class Laptop
+{
+	String brandname;
+	String modelname;
+	int price;
+	int warranty;  
+}
+
+class SportMan
+
+{
+	String name;
+	int age;
+	double height;
+	double weight;
+	String country;
+	String sportstype; 
+	
+	
+}
+
+class Testcase
+{
+String Testcasename;
+String Creator;
+String Setps;
+
+
+}
+
+
+class Car
+{
+	String name;
+	String modelname;
+	String cartype;
+	int price;
+	int warranty;  
+}
+
+
+class Pen
+{
+	String name;
+	String pentype;
+	double height;
+	double weight;
+	int price;
+	  
+}
+
+class guitar
+{
+	String name;
+	String guitartype;
+	int noOfcodes;
+	int price;
+	int warranty;  
+}
+
+class cooler
+{
+	String companyname;
+	String coolertype;
+	double height;
+	double weight;
+	int price;
+	int warranty;
+	
+}
+
+class TV 
+{
+	String Companyname;
+	String modelname;
+	String type;
+	int price;
+	int warranty;
+	
+}
+
