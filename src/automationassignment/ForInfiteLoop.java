75d848a91c1659a35d3750d612db0b6bc58d59c7
PratapChauhan <PratapChauhan@host.docker.internal>
2023-10-18 12:51:08 +0530

    ForInfiteLoop commited

---
+++ src/automationassignment/ForInfiteLoop.java
@@ -0,0 +1,20 @@
+package automationassignment;
+
+
+public class ForInfiteLoop {
+	public static void main(String[] args)
+	
+	{
+	
+		for(int i=0; i<3; --i)
+		System.out.println("This is For infiteloop ");
+		
+		 boolean i= true;
+		 
+		for (; i; ) {
+            System.out.println("hello");
+        } 
+	
+	}
+	
+}
