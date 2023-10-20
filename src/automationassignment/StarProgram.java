5c3d03b9aa465eeb4b826e9b36b91c8ab6a00cb6
PratapChauhan <PratapChauhan@host.docker.internal>
2023-10-20 09:38:17 +0530

    StarPrograme

---
+++ src/automationassignment/StarProgram.java
@@ -0,0 +1,339 @@
+package automationassignment;
+
+public class StarProgram {
+
+	public static void main(String[] args) {
+		
+		int i,j, k, l;
+		
+		
+		/*	for(i=1; i<=5; i++)
+		{
+			for (j=1; j<=i; j++)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			
+		}
+	 
+		 Out Put
+		 Pattern 1
+		 *
+		 * *
+		 * * *
+		 * * * *
+		 * * * * *		 
+		  
+		  
+		  
+		  */
+		
+		/* for(i=1; i<=5; i++)
+		{
+			
+			for (j=4; j>=i; j--)
+			{
+				System.out.print(" ");
+			}
+			
+			for (k=1; k<=i; k++)
+			{
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+		
+		out put
+				 Pattern 2
+
+	     *
+	    **
+	   ***
+	  ****
+	 *****
+	 */
+		
+		
+				
+		/*	for(i=1; i<=5; i++)
+		{
+			for (j=5; j>=i; j--)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			
+		}
+		
+	 
+		 Out Put
+		 Pattern 3
+		 
+		 * * * * *
+		 * * * *		 
+		 * * * 
+		 * * 
+		 * 
+		 
+		  */
+		 
+		
+	/*	 for(i=1; i<=5; i++)
+			{
+				
+				for (j=5; j>=i; j--)
+				{
+					System.out.print("*");
+				}
+				
+				System.out.println();
+				
+				for (k=1; k<=i; k++)
+				{
+					System.out.print(" ");
+					
+				}
+				
+			}	
+		
+		
+		OUT PUT Pattern 4
+		*****
+		 ****
+		  ***
+		   **
+		   	*
+		
+		*/
+		  
+	
+	/*	for(i=1; i<=5; i++)
+		{
+			
+			for (j=4; j>=i; j--)
+			{
+				System.out.print(" ");
+			}
+			
+			for (k=1; k<=i; k++)
+			{
+				System.out.print("*");
+				
+			}
+			
+			for (l=2; l<=i; l++)
+			{
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}	
+		
+	Out put	Pattern 5
+		  	*
+		   ***
+		  *****
+		 *******
+		*********
+		*/
+		
+	/*	 for(i=1; i<=5; i++)
+			{
+				
+				for (j=5; j>=i; j--)
+				{
+					System.out.print("*");
+				}
+				for (l=4; l>=i;l--)
+				{
+					System.out.print("*");
+				}
+				
+				System.out.println();
+				
+				for (k=1; k<=i; k++)
+				{
+					System.out.print(" ");
+					
+				}
+				
+			}	
+	
+		
+OUT PUT Pattern 6
+*********
+ *******
+  *****
+   ***
+    *
+		
+		
+		*/	
+		
+		/*for(i=1; i<=5; i++)
+		{
+			
+			for (j=4; j>=i; j--)
+			{
+				System.out.print(" ");
+			}
+			
+			for (k=1; k<=i; k++)
+			{
+				System.out.print("*");
+				
+			}
+			
+			for (l=2; l<=i; l++)
+			{
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+		
+		 for(i=1; i<=5; i++)
+			{
+				
+				for (j=5; j>=i; j--)
+				{
+					System.out.print("*");
+				}
+				for (l=4; l>=i;l--)
+				{
+					System.out.print("*");
+				}
+				
+				System.out.println();
+				
+				for (k=1; k<=i; k++)
+				{
+					System.out.print(" ");
+					
+				}
+				
+			} 
+			
+			Pattern 7 - doubt for one more row is being printed 
+	*
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+			
+			*/
+
+		
+		
+	/*	for(i=1; i<=5; i++)
+		{
+			for(j=1;j<=i;j++)
+			{System.out.print("*");
+				
+			}
+			System.out.println();
+				
+		}
+		
+		for(i=1; i<=4; i++)
+		{
+			for(j=4; j>=i; j--)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			
+			
+		}
+		OUT PUT Pattern 8
+		
+		*
+		**
+		***
+		****
+		*****
+		****
+		***
+		**
+		*
+	
+		*/
+		
+		
+		
+	/*	for(i=1;i<=5;i++)
+		{
+			for(j=4;j>=i;j--)
+			{
+				System.out.print(" ");
+				
+			}
+			for (k=1; k<=i; k++)
+			{
+				System.out.print("*");
+				
+			}
+			System.out.println();
+			
+		}	
+		
+		
+		for(i=1; i<=4; i++)
+		{
+			System.out.print(" ");
+		
+		
+		
+			
+			for (j=4; j>=i; j--)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			
+			for (k=1; k<=i; k++)
+			{
+				System.out.print(" ");
+				
+			}
+			
+		}
+	
+		Pattern 9
+		
+		 *
+	    **
+	   ***
+	  ****
+	 *****
+	  ****
+	   ***
+	    **
+	     *
+		
+		*/
+		
+	
+		
+		
+		
+		}
+		
+		
+		}
+		
+		
+	
+	
+
