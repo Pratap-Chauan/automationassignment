cba756f887458eb5c33f1dc185e6368e037032a3
PratapChauhan <PratapChauhan@host.docker.internal>
2023-10-20 10:30:34 +0530

    VariableSwap

---
+++ src/automationassignment/VariableSwap.java
@@ -0,0 +1,30 @@
+package automationassignment;
+
+public class VariableSwap {
+	
+	public static void main(String[] args){
+
+	    int k;
+		int i = 10;
+
+	    int j = 7;
+	    
+	    
+	    
+	    k=i;
+	    i=j;
+	    j=k;
+	    
+	    
+
+	    System.out.println("value of i is " +i);
+
+	   
+	  
+	    System.out.println("value of j is "+ j);
+
+	      
+
+	   }
+
+}
