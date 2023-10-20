aaee08c16b4f698c0b9da75904c1614bd70e52c2
PratapChauhan <PratapChauhan@host.docker.internal>
2023-10-20 10:06:12 +0530

    SumOf17 committed

---
+++ src/automationassignment/SumOf17.java
@@ -0,0 +1,27 @@
+package automationassignment;
+
+public class SumOf17 {
+	
+	
+
+
+		 public static void main(String[] args) {
+
+		 
+		 int i,j,k,l;
+		 i= 1;
+		 j= 2;
+		 k= 6;
+		 l= 7;
+		 int sum = ((k-i)*j) + l;
+		 
+		 System.out.println(sum);
+		 
+		 }
+
+
+		}
+
+	
+
+
