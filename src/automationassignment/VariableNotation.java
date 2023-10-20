c264a7c39c1533a06d159c6e653e48433853ef5e
PratapChauhan <PratapChauhan@host.docker.internal>
2023-10-20 10:20:02 +0530

    VariableNotation commit

---
+++ src/automationassignment/VariableNotation.java
@@ -0,0 +1,20 @@
+package automationassignment;
+
+public class VariableNotation {
+	
+	
+	int i = 10; 
+
+	int int = 20; // Syntax error on token "int", invalid VariableDeclaratorId
+
+	int goto = 30; //Syntax error on token "goto", invalid VariableDeclaratorId
+
+	int default = 40; // Syntax error on token "default", invalid VariableDeclaratorId
+
+	int class = 50; // Syntax error on token "class", invalid VariableDeclaratorId
+
+	int j = 70;
+
+	
+	//So as we can see - i and j notation are correct 
+}
