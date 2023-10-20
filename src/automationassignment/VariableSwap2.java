8ca07ba34b3a4fc57bbd356c0f19f705f2945eed
PratapChauhan <PratapChauhan@host.docker.internal>
2023-10-20 10:45:29 +0530

    VariableSwap swap values without using third variable

---
+++ src/automationassignment/VariableSwap2.java
@@ -0,0 +1,25 @@
+package automationassignment;
+
+public class VariableSwap2 {
+	
+	public static void main(String[] args){
+
+	    
+		int i = 10;
+	    int j = 7;
+	    
+	    i=i+j;
+	    j=i-j;
+	    i=i-j;
+
+	    System.out.println("value of i is " +i);
+
+	   
+	  
+	    System.out.println("value of j is "+ j);
+
+	      
+
+	   }
+
+}
