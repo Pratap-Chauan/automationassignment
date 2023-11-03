42631902245aaea62f133cfe3268c61ec393d9a6
PratapChauhan <PratapChauhan@host.docker.internal>
2023-11-03 11:08:43 +0530

    Methodreturn commit

---
+++ src/Objects/Methodreturn.java
@@ -0,0 +1,34 @@
+package Objects;
+
+
+public class Methodreturn {
+	public static void main(String[] args)
+	{
+		int n1=5;
+		int n2=6;
+		calculator3 addi =new calculator3();
+		int result = addi.add(n1, n2);
+		System.out.println(result);
+		int result1 = calculator3.minus(n2, 1);
+		System.out.println(result1);
+	}
+
+}
+
+class calculator3
+{
+	public int add(int i, int j)
+	{
+		int r = i+j;
+		
+		return r;
+	}
+	
+	public static int minus(int o, int p)
+	{
+		int g = o-p;
+		
+		return g;
+	}
+	
+}
