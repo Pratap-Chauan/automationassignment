01d6b5049754af2b15689ef4ccda7037b141d647
PratapChauhan <PratapChauhan@host.docker.internal>
2023-11-05 16:07:56 +0530

    CharCount committed

---
+++ src/StringDemo/CharCount.java
@@ -0,0 +1,29 @@
+package StringDemo;
+
+import java.util.Scanner;
+
+public class CharCount {
+	public static void main(String[] args) {
+		
+		
+		 Scanner input = new Scanner(System.in); 
+		 System.out.println("Enter the String");
+		 String str = input.nextLine();
+		 
+		
+		//String str = "this is java";
+		int count = 0;
+		
+		for(int i=0; i<str.length();i++)
+		{
+			if(str.charAt(i)!=' ')
+				count++;
+					
+			
+		}
+		
+		System.out.println("Character count in String are "+count); 
+		
+	}
+
+}
