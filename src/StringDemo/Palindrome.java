54737ca6f6e504fd7198efeef47415c0267669d7
PratapChauhan <PratapChauhan@host.docker.internal>
2023-11-28 10:43:27 +0530

    Palindrome

---
+++ src/StringDemo/Palindrome.java
@@ -0,0 +1,25 @@
+package StringDemo;
+
+import java.util.Scanner;
+
+public class Palindrome {
+	
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter the string\n");
+		
+		String ss = input.next();
+		
+		//String ss = "123";
+		StringBuffer sbr = new StringBuffer(ss);
+		sbr.reverse();
+		System.out.println(sbr);
+	
+		
+	}
+	
+	
+
+}
