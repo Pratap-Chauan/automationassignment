da56dbbe2918125100d760c5f7f8de0e83541077
PratapChauhan <PratapChauhan@host.docker.internal>
2024-02-01 11:40:29 +0530

    PrimeNumberList

---
+++ src/automationassignment/PrimeNumberList.java
@@ -0,0 +1,34 @@
+package automationassignment;
+
+
+
+public class PrimeNumberList {
+	
+	public static void main(String[] args)
+	{
+		int count =0,n=0,i=1,j=1;
+		
+		while(n<20)			
+		{
+			j=1;
+			count=0;
+			while(j<=i)
+			{
+				if(i%j==0)
+					count++;
+					j++;
+					
+			}
+			
+			if (count==2)
+			{
+				System.out.println(i);
+				n++;;
+				
+			}
+			i++;
+		}
+		
+	}
+
+}
